21f671250356fbbf0ce033cb00b3dc2df8c8acbf
Vemund Fredriksen <vemund.fredriksen@hotmail.com>
2023-12-09 17:24:21 +0100

    Implement power units and fix bug with total power calculation

--- src/ExpressiveFit/Models/Activity/PowerCharacteristics.cs
+++ src/ExpressiveFit/Models/Activity/PowerCharacteristics.cs
@@ -4,10 +4,10 @@ public record PowerCharacteristics
 {
     public List<TickTuple<int>> Series { get; init; }
     public bool SeriesIsComplete { get; set; }
-    public int Max { get; init; }
-    public int Min { get; init; }
-    public double Average { get; init; }
-    public int TotalPower { get; init; }
+    public PowerValue Max { get; init; }
+    public PowerValue Min { get; init; }
+    public PowerValue Average { get; init; }
+    public WorkValue TotalPower { get; init; }
 
     public PowerCharacteristics(List<Tick> ticks)
     {
@@ -16,9 +16,41 @@ public record PowerCharacteristics
 
         Series = ticks.Where(t => t.Power is not null).Select(t => new TickTuple<int>(t.Timestamp, t.Power!.Value)).ToList();
         SeriesIsComplete = Series.Count == ticks.Count;
-        Max = ticks.Max(t => t.Power)!.Value;
-        Min = ticks.Min(t => t.Power)!.Value;
-        Average = ticks.Average(t => t.Power)!.Value;
-        TotalPower = ticks.Sum(t => t.Power)!.Value;
+        Max = new PowerValue(ticks.Max(t => t.Power)!.Value);
+        Min = new PowerValue(ticks.Min(t => t.Power)!.Value);
+        Average = new PowerValue(ticks.Average(t => t.Power)!.Value);
+        TotalPower = CalculateTotalWattHours(ticks);
+    }
+
+    private WorkValue CalculateTotalWattHours(List<Tick> ticks)
+    {
+        var duration = ticks.Max(t => t.Timestamp) - ticks.Min(t => t.Timestamp);
+        var averageWatts = ticks.Average(t => t.Power) ?? 0;
+        return new(averageWatts * duration.TotalHours);
+    }
+}
+
+public record PowerValue
+{
+    private readonly double _watts;
+    public double Watts => _watts;
+    public double HorsePowers => _watts * 0.00134102209;
+
+    public PowerValue(double watts)
+    {
+        _watts = watts;
+    }
+}
+
+public record WorkValue
+{
+    private readonly double _wattHours;
+    public double WattHours => _wattHours;
+    public double Joules => _wattHours * 3600;
+    public double KiloJoules => Joules / 1000;
+
+    public WorkValue(double wattHours)
+    {
+        _wattHours = wattHours;
     }
 }
