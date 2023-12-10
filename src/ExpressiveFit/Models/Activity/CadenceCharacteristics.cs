6238723367a8258d8cd630c3b7c282961c9e5c12
Vemund Fredriksen <vemund.fredriksen@hotmail.com>
2023-12-10 20:24:22 +0100

    Use total steps per minute rather than total steps per leg per minute for cadence

--- src/ExpressiveFit/Models/Activity/CadenceCharacteristics.cs
+++ src/ExpressiveFit/Models/Activity/CadenceCharacteristics.cs
@@ -12,9 +12,9 @@ public record CadenceCharacteristics
         if (!ticks.Exists(t => t.Cadence != null))
             throw new ArgumentException("The list of ticks is missing cadence data!", nameof(ticks));
 
-        Series = ticks.Where(t => t.Cadence is not null).Select(t => new TickTuple<int>(t.Timestamp, t.Cadence!.Value)).ToList();
-        Max = ticks.Max(t => t.Cadence)!.Value;
-        Min = ticks.Min(t => t.Cadence)!.Value;
-        Average = ticks.Average(t => t.Cadence)!.Value;
+        Series = ticks.Where(t => t.Cadence is not null).Select(t => new TickTuple<int>(t.Timestamp, t.Cadence!.Value * 2)).ToList();
+        Max = 2 * ticks.Max(t => t.Cadence)!.Value;
+        Min = 2 * ticks.Min(t => t.Cadence)!.Value;
+        Average = 2 * ticks.Average(t => t.Cadence)!.Value;
     }
 }
